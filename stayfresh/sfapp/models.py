a44274b1a3de5a4e4668233852fb9f73044ee364
Ian Pun <itpun@sfu.ca>
2018-03-14 13:31:21 -0700

    made some changes to models to include some client information

--- stayfresh/sfapp/models.py
+++ stayfresh/sfapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+	ID        = models.CharField('ID', max_length=50)
+	firstName = models.CharField('First Name', max_length=50)
+	lastName  = models.CharField('Last Name', max_length=50)
+	notes     = models.CharField('Notes', max_length=500)
+
+	def __str__(self):
+		return self.ID
+
+class Transaction(models.Model):
+	SIZES = (
+		('S', 'Small'),
+		('M', 'Medium'),
+		('L', 'Large'),
+		('XL', 'Extra Large')
+	)
+	SALE_TYPE = (
+		('C', 'Cosignment'),
+		('B', 'Buyout'),
+		('S', 'Sale'),
+		)
+	customer  = models.ForeignKey(Customer,on_delete=models.CASCADE)
+	size      = models.CharField(max_length=1, choices=SIZES)
+	sale_type = models.CharField(max_length=1, choices=SALE_TYPE)
+	Price     = models.PositiveIntegerField()
